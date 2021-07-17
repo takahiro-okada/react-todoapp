eeddbe84b76c153f643efb101b204067b174ef0f
takahiro okada <t.okada@koshikiad.com>
2021-07-17 09:00:43 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>HOGE</h1>
+      </header>
+    </div>
+  );
+}
+export default App;
