c138b0b416b78afc7ca2ded90a564a64508d6c82
takahiro okada <t.okada@koshikiad.com>
2021-07-24 04:41:41 +0900

    modify styled-component props

--- src/components/atoms/BoxTitle.jsx
+++ src/components/atoms/BoxTitle.jsx
@@ -2,39 +2,6 @@ import styled from "styled-components";
 
 export const BoxTitle = (props) => {
 	const { taskLength } = props;
-	const SBoxTitle = styled.div`
-		position: relative;
-		display: flex;
-	`;
-	const STitle = styled.h2`
-		font-family: "Staatliches", cursive;
-		font-size: 40px;
-		font-weight: bold;
-		padding-left: 20px;
-		letter-spacing: 0.1em;
-	`;
-	const STitleCircle = styled.span`
-		width: 55px;
-		height: 55px;
-		background-color: ${props.color};
-		border-radius: 100%;
-		display: block;
-		position: absolute;
-		z-index: -1;
-		position: absolute;
-		top: 50%;
-		left: 0;
-		transform: translate(0%, -50%);
-	`;
-	const STitleNumber = styled.span`
-		font-size: 65px;
-		position: absolute;
-		top: 50%;
-		right: 0;
-		transform: translate(0, -50%);
-		font-weight: bold;
-		font-family: "Dancing Script", cursive;
-	`;
 	return (
 		<SBoxTitle>
 			<STitleCircle></STitleCircle>
@@ -43,3 +10,37 @@ export const BoxTitle = (props) => {
 		</SBoxTitle>
 	);
 };
+
+const SBoxTitle = styled.div`
+	position: relative;
+	display: flex;
+`;
+const STitle = styled.h2`
+	font-family: "Staatliches", cursive;
+	font-size: 40px;
+	font-weight: bold;
+	padding-left: 20px;
+	letter-spacing: 0.1em;
+`;
+const STitleCircle = styled.span`
+	width: 55px;
+	height: 55px;
+	background-color: ${(props) => props.color};
+	border-radius: 100%;
+	display: block;
+	position: absolute;
+	z-index: -1;
+	position: absolute;
+	top: 50%;
+	left: 0;
+	transform: translate(0%, -50%);
+`;
+const STitleNumber = styled.span`
+	font-size: 65px;
+	position: absolute;
+	top: 50%;
+	right: 0;
+	transform: translate(0, -50%);
+	font-weight: bold;
+	font-family: "Dancing Script", cursive;
+`;
