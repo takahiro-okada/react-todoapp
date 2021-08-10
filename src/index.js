8c8e4ee6254aae6a3167497c80d8653e1e89ad4e
takahiro okada <t.okada@koshikiad.com>
2021-08-11 07:31:47 +0900

    update

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ ReactDOM.render(
 	<Auth0Provider
 		domain="dev-9jp-2yxk.jp.auth0.com"
 		clientId="guhIXOHryY6O9zuNSeXmdwpZhQ6oEMc6"
-		redirectUri="https://localhost:3000/"
+		redirectUri="https://localhost:3000/,https://react-osyare-todoapp-liard.vercel.app/"
 	>
 		<BrowserRouter>
 			<App />
